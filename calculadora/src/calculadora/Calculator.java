d54a0ca7e3e350c11fbef4834a0be4818ac73a39
Victor Miranda <victor.miranda.alcantara12@gmail.com>
2019-06-09 22:26:05 +0200

    Añadido el método sumar a la calculadora

--- calculadora/src/calculadora/Calculator.java
+++ calculadora/src/calculadora/Calculator.java
@@ -17,6 +17,36 @@ public class Calculator {
 			System.out.println("[1] Sumar [2] Restar [3] Multiplicar [4] Dividir [5] Información ");
 			opt = Leer.datoInt();
 
+			switch (opt) {
+			case 1:
+				System.out.println("Inserte numero a");
+				a = Leer.datoInt();
+				System.out.println("Inserte numero b");
+				b = Leer.datoInt();
+				result = a + b;
+				System.out.println("El resultado es "+result);
+
+				break;
+			case 2:
+
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+
+			case 5:
+				System.out.println("En esta calculadora deberás elegir una opción tras eso, deberas elegir dos numero, siendo a"
+						+ "el primer numero de la operación");
+				break;
+
+			default:
+				System.out.println("Esa opción no existe");
+				break;
+			}
+
 		} while (opt != 0);
 
 	}
