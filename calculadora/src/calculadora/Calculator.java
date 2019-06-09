7625de0d193ce45f1beec87093e301fd29504659
Victor Miranda <victor.miranda.alcantara12@gmail.com>
2019-06-09 22:33:01 +0200

    Añadido método multiplicar a la calculadora

--- calculadora/src/calculadora/Calculator.java
+++ calculadora/src/calculadora/Calculator.java
@@ -38,6 +38,13 @@ public class Calculator {
 				break;
 			case 3:
 
+				System.out.println("Inserte numero a");
+				a = Leer.datoInt();
+				System.out.println("Inserte numero b");
+				b = Leer.datoInt();
+				result = a * b;
+				System.out.println("El resultado es "+result);
+
 				break;
 			case 4:
 
